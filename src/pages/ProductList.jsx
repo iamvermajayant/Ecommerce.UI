21a5879799cdfcb8ffaa9787e8fe815a157ed883
Jayant Verma <iamvermajayant@gmail.com>
2024-06-04 23:46:50 +0530

    added ProductList component

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -7,18 +7,33 @@ import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
 
-const FilterContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
+const Title = styled.h1`
+  margin: 20px;
 `;
 
-const Title = styled.h1`
-    margin: 20px;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Filter = styled.div`
-    margin: 20px;
-`
+  margin: 20px;
+  
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  
+`;
+const Option = styled.option``;
 
 const ProductList = () => {
   return (
@@ -27,12 +42,42 @@ const ProductList = () => {
       <Announcements />
       <Title>Dresses</Title>
       <FilterContainer>
-        <Filter>Filter</Filter>
-        <Filter>Filter2</Filter>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
       </FilterContainer>
-      <Products/>
-      <Newsletter/>
-      <Footer/>
+      <Products />
+      <Newsletter />
+      <Footer />
     </Container>
   );
 };
